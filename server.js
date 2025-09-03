da72c192ae8614cf9adaf3462ddcf1eabcedd3cb
dinuthfernandez <dinuthchamikara@icloud.com>
2025-09-03 19:06:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const rateLimit = require('express-rate-limit');
 const nodemailer = require('nodemailer');
 
 const app = express();
